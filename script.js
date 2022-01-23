4954ca8e2276862aac07ac0d303e2a58b5e6c777
Joseph <joe@uhhuhohyeah.com>
2022-01-23 14:03:07 -0800

    set up click functionality on color squares

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 "use strict";
 
+const green = document.querySelector(".green");
+const yellow = document.querySelector(".yellow");
+const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+
+let selection;
+
+const checkClick = function () {
+  console.log(`${selection} clicked!`);
+};
+
 // Generate random color
 
 const color = Math.trunc(Math.random() * 4) + 1;
 console.log(color);
+
+green.addEventListener("click", function () {
+  selection = "green";
+  checkClick();
+});
+yellow.addEventListener("click", function () {
+  selection = "yellow";
+  checkClick();
+});
+red.addEventListener("click", function () {
+  selection = "red";
+  checkClick();
+});
+blue.addEventListener("click", function () {
+  selection = "blue";
+  checkClick();
+});
